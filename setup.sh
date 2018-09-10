f4d53b05203e6438adbe771b73f312d95ae500cf
Quang Le <quang@dwarvesv.com>
2018-09-10 09:55:41 +0700

    Add twilio credentials

--- setup.sh
+++ setup.sh
@@ -6,10 +6,13 @@ sleep 2
 PRIVATE_IP="127.0.0.1"
 
 curl -X PUT --url $PRIVATE_IP:8500/v1/kv/sendgrid -d $SENDGRID_API_KEY 
+curl -X PUT --url $PRIVATE_IP:8500/v1/kv/twilio \
+    -d '{
+    "sid":"'$TWILIO_SID'",
+    "key":"'$TWILIO_KEY'"
+}'
 curl -X PUT --url $PRIVATE_IP:8500/v1/kv/sql \
     -d '{
-    "host": "yggdrasil_postgres",
-    "port": 5432,
     "user": "admin",
     "password": "123"
 }'
