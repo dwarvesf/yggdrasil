4dd3bd2f1f72c94be4e44923b79850081158fb15
minhkhiemm <vominhkhiem212@gmail.com>
2018-09-13 10:01:04 +0700

    Delete json annotation

--- sms/model/request.go
+++ sms/model/request.go
@@ -3,9 +3,9 @@ package model
 // Request is a struct define request message taken from queue
 type Request struct {
 	From     string
-	To       string `json:"to"`
-	Provider string `json:"provider"`
-	Content  string `json:"content"`
+	To       string
+	Provider string
+	Content  string
 }
 
 //TwilioSecret is a struct that define App sid, authenticate token and app number from consul
