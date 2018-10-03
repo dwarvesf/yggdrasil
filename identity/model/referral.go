83b3b3f2e5a0e8d9cd9db888ae7047d76685db92
huynguyenh <hoanghuy2908@gmail.com>
2018-10-03 11:54:12 +0700

    add struct tag

--- identity/model/referral.go
+++ identity/model/referral.go
@@ -10,9 +10,9 @@ import (
 
 type Referral struct {
 	ID          uuid.UUID      `json:"id" gorm:"not null"`
-	TTL         time.Time      `json:"ttl"`
-	FromUserID  uuid.UUID      `json:"from_user_id"`
-	Code        string         `json:"code"`
-	ToUserEmail string         `json:"to_user_email"`
+	TTL         time.Time      `json:"ttl" gorm:"not null"`
+	FromUserID  uuid.UUID      `json:"from_user_id" gorm:"not null"`
+	Code        string         `json:"code" gorm:"not null"`
+	ToUserEmail string         `json:"to_user_email" gorm:"not null"`
 	Metadata    postgres.Jsonb `json:"metadata"`
 }
