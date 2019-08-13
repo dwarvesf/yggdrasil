2bc5be3c95b006fb230b0edcf7ddc9dfd4853641
huynguyenh <hoanghuy2908@gmail.com>
2019-08-13 15:28:12 +0700

    add missing import

--- services/notification/service/notification.go
+++ services/notification/service/notification.go
@@ -2,9 +2,11 @@ package notification
 
 import (
 	"context"
+
+	"github.com/dwarvesf/yggdrasil/services/notification/model"
 )
 
 // Notificationer ..
 type Notificationer interface {
-	Send(ctx context.Context, devices []DeviceToken, body, title string, data interface{}) error
+	Send(ctx context.Context, devices []model.DeviceToken, body, title string, data interface{}) error
 }
